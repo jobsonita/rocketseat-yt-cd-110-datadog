d822ce130816c6819ff81cf7d76e041fcae1b902
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2021-11-20 18:56:03 -0300

    Add a stub route for creating users

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import crypto from "crypto";
 
 const app = express();
 
@@ -7,8 +8,26 @@ const serverHttp = http.createServer(app);
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.json({ message: "Hello Datadog" });
+app.post("/users", (request, response) => {
+  const { token } = request.headers;
+
+  const { name, instagram } = request.body;
+
+  if (!token) {
+    return response.status(401).end();
+  }
+
+  if (!name || !instagram) {
+    return response.status(500).end({ error: "Invalid parameters" });
+  }
+
+  const user = {
+    id: crypto.randomBytes(16).toString("hex"),
+    name: name,
+    username: instagram,
+  };
+
+  return response.json(user);
 });
 
 export { serverHttp };
